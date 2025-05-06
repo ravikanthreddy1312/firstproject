e9a7a9d97de62f4d68cf6a401edc9c29c761e8a3
Ravikanth reddy Surireddy <surireddyravi5544@gmail>
2025-05-05 20:49:25 -0500

    Updates

---
+++ precache-manifest.29c0ecd59061628b266759702c62d605.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce43d28ac9ee418123b8adcd9fcd0974",
+    "url": "/bonus/index.html"
+  },
+  {
+    "revision": "36288391bef461cebf9e",
+    "url": "/bonus/static/css/2.5417e51f.chunk.css"
+  },
+  {
+    "revision": "1806f7d2dcdc39de7740",
+    "url": "/bonus/static/css/main.e327af0c.chunk.css"
+  },
+  {
+    "revision": "36288391bef461cebf9e",
+    "url": "/bonus/static/js/2.29514214.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/bonus/static/js/2.29514214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478623a2a23e2ec4e66e",
+    "url": "/bonus/static/js/3.fe05a9fe.chunk.js"
+  },
+  {
+    "revision": "1806f7d2dcdc39de7740",
+    "url": "/bonus/static/js/main.0330714a.chunk.js"
+  },
+  {
+    "revision": "dfb935a02bf5573d5327",
+    "url": "/bonus/static/js/runtime-main.25797d43.js"
+  },
+  {
+    "revision": "e8ec7a3407e23652f0b12687389ace1f",
+    "url": "/bonus/static/media/bridge.e8ec7a34.jpg"
+  },
+  {
+    "revision": "66d9136b5ecddc46cdeda2832b3250ff",
+    "url": "/bonus/static/media/job.66d9136b.jpg"
+  },
+  {
+    "revision": "25e22d30f09f4d717893bd0fe44eb290",
+    "url": "/bonus/static/media/profile.25e22d30.jpg"
+  },
+  {
+    "revision": "f58433ffee984013e60360e70bcaf0cc",
+    "url": "/bonus/static/media/restaurant.f58433ff.jpg"
+  }
+]);
